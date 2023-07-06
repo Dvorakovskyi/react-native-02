8f9af6e68f1eac2504055d6d1bc93d13076766b3
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-07-06 17:16:01 +0300

    Update RegistrationScreen.jsx

--- AwesomeProject/src/components/RegistrationScreen.jsx
+++ AwesomeProject/src/components/RegistrationScreen.jsx
@@ -1,11 +1,22 @@
-import {View, ImageBackground } from "react-native";
+import {StyleSheet, View, Text, TextInput } from "react-native";
 
 const RegistrationScreen = () => {
   return (
-    <View>
-      <ImageBackground></ImageBackground>
+    <View style={styles.conteiner}>
+      <Text>Руєстріція</Text>
+      <TextInput keyboardType="text" placeholder="Логін"/>
+      <TextInput keyboardType="email-address" placeholder="Адреса електронної пошти"/>
+      <TextInput keyboardType="password" placeholder="Пароль"/>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  conteiner: {
+    marginTop: '100%',
+    paddingTop: 92,
+    backgroundColor: 'red',
+  },
+})
 
 export default RegistrationScreen;
